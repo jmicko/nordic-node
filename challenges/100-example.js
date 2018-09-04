33f20c07a4e642168f431883b044546f3b9c735e
Luke Schlangen <lukeschlangen@gmail.com>
2018-09-04 15:29:33 -0500

    add node objects test

--- challenges/100-example.js
+++ challenges/100-example.js
@@ -1,4 +1,7 @@
 // Can you run this code?
+// You need to have the right path to run the file
+// in the root you can say `node file-name.js` and that will work
+// but this is in a sub-folder, so you will need `node sub-folder/file-name.js`
 // What did you need to type in the terminal in order to run this code?
 
-console.log('running example.js This is node!');
+console.log('running 100-example.js This is node!');
--- test.js
+++ test.js
@@ -1,8 +1,15 @@
 const nodeNumbers = require('./challenges/110-node-numbers');
+const nodeObjects = require('./challenges/120-node-objects');
 
 if (nodeNumbers == 20) {
+    console.log('Passed nodeNumbers!');
+} else {
+    console.log(`XXXXXXXXXXX FAILED nodeNumbers XXXXXXXXXXX`)
+}
+
+if (nodeObjects.boxes == 20) {
     console.log('Passed!');
 } else {
-    console.log(`FAILED!`)
+    console.log(`XXXXXXXXXXX FAILED nodeObjects XXXXXXXXXXX`)
 }
 
