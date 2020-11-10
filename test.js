8f3f5fd96cce62456fa711db9930a93bd6ad2404
John Micko <jrmicko5012@gmail.com>
2020-11-10 12:11:55 -0600

    test accounts for 140 stretch

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 const nodeNumbers = require('./challenges/110-node-numbers');
 const nodeObjects = require('./challenges/120-node-objects');
 const nodeFunction = require('./challenges/130-node-function');
+const stretchVialFinder = require('./challenges/140-stretch-vial-finder');
+
+const emptyBox = [];
+const singleBox = ['vial', 'sandwich', 'dog food']
+const recursiveBox = [['vial', ['dog food', 'dog food'], 'taco'], 'vial', ['vial', 'vial', ['vial']]]
+
 
 if (nodeNumbers == 20) {
     console.log('Passed nodeNumbers test!');
@@ -35,6 +41,47 @@ if (nodeFunction(8) == 6) {
     XXXXXXXXXXX
     FAILED nodeFunction
     function should return 6 when 8 is passed
+    XXXXXXXXXXX`);
+}
+
+// duplicate/change test to acount for situations with a fraction of a dog
+if (nodeFunction(9) == 7) {
+    console.log('Passed nodeFunction test 3!');
+} else {
+    console.log(`
+    XXXXXXXXXXX
+    FAILED nodeFunction
+    function should return 7 when 9 is passed
+    XXXXXXXXXXX`);
+}
+
+if (stretchVialFinder(emptyBox) == 0) {
+    console.log('Passed stretchVialFinder stretch goal test 1!');
+} else {
+    console.log(`
+    XXXXXXXXXXX
+    FAILED stretchVialFinder test 1
+    function should return 0 when emptybox is passed
+    XXXXXXXXXXX`);
+}
+
+if (stretchVialFinder(singleBox) == 1) {
+    console.log('Passed stretchVialFinder stretchVialFinder test 2!');
+} else {
+    console.log(`
+    XXXXXXXXXXX
+    FAILED stretchVialFinder test 2
+    function should return 1 when singleBox is passed
+    XXXXXXXXXXX`);
+}
+
+if (stretchVialFinder(recursiveBox) == 5) {
+    console.log('Passed stretchVialFinder stretch goal test 3!');
+} else {
+    console.log(`
+    XXXXXXXXXXX
+    FAILED stretchVialFinder test 3
+    function should return 5 when recursiveBox is passed
     XXXXXXXXXXX`);
 }
 
